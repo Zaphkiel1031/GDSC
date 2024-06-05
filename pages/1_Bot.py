6132f290575109479814cf671a3c7554dc40ef5a
Zaphkiel1031 <68491362+Zaphkiel1031@users.noreply.github.com>
2024-06-05 14:38:19 +0000

    Initial commit

--- pages/1_Bot.py
+++ pages/1_Bot.py
@@ -1,5 +1,6 @@
 import requests
 import streamlit as st
+import streamlit.components.v1 as components
 
 # GIF 圖片的原始 URL
 gif_url = "https://raw.githubusercontent.com/j7808833/test_02/main/pic/Cyberpunk_bar_05.gif"
@@ -69,3 +70,38 @@ if question := st.chat_input("請輸入問題..."):
         )
     with st.chat_message("assistant"):
         st.markdown(response_txt)
+
+
+html_code = """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Coze Bot Integration</title>
+  <script src="https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/0.1.0-beta.2/libs/oversea/index.js"></script>
+</head>
+<body>
+  <div id="coze-chat"></div>
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      try {
+        new CozeWebSDK.WebChatClient({
+          config: {
+            bot_id: '7376255263235424257',  
+          },
+          componentProps: {
+            title: 'Coze',  
+            lang: 'zh-TW'  
+          },
+        });
+        console.log("Coze Web SDK initialized successfully.");
+      } catch (error) {
+        console.error("Error initializing Coze Web SDK:", error);
+        document.getElementById("coze-chat").innerText = "Error initializing Coze Web SDK: " + error.message;
+      }
+    });
+  </script>
+</body>
+</html>
+"""
+
+components.html(html_code, height=600)
